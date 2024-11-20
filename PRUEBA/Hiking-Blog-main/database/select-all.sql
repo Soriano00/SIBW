04931021de3e364f1b3f8830ef4fbf2c58c7b78b
alex <alexsoriano00@gmail.com>
2024-11-20 15:23:13 +0100

    Removiendo carpetas zip

--- PRUEBA/Hiking-Blog-main/database/select-all.sql
+++
@@ -1,8 +0,0 @@
-USE SIBW;
-
-SELECT * FROM events;
-SELECT * FROM comments;
-SELECT * FROM gallery;
-SELECT * FROM tags;
-SELECT * FROM tags_events;
-SELECT * FROM users;
--- PRUEBA/Hiking-Blog-main/www/php/pages/login.php
+++
@@ -1,4 +0,0 @@
-<?php
-    unset($_SESSION['email']);
-    echo $twig->render('login.html',[] );
-?>
